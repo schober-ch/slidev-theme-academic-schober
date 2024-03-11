dc74349ae03fafff8ecb0a65a7c0a9f050ab3155
Christoph Schober <mail@christoph-schober.de>
2024-03-11 14:17:44 +0100

    feat: verison with pdf lib

---
+++ components/PDFView.vue
@@ -0,0 +1,27 @@
+<template>
+<VuePDF :key="'page_'+props.page" v-if="showPdf" fit-parent :page="props.page" :pdf="pdf"/>
+</template>
+
+<script setup lang="ts">
+import { VuePDF, usePDF } from '@tato30/vue-pdf'
+import { ref } from 'vue'
+
+const showPdf = ref(false);
+
+const props = defineProps<{
+  src: string;
+  page: number;
+}>();
+
+console.log("XXX PDFView for page " + props.page)
+let { pdf, pages, info } = usePDF(props.src)
+console.log(pdf);
+console.log(info.value);
+
+
+setTimeout(() => {
+  showPdf.value = true;
+  console.log("set true")
+}, 1000);
+
+</script>
---
+++ layouts/pdf-page.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="slidev-layout">
+    <PDFView class="" :src="props.pdf" :page="page"></PDFView>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+  pdf: {
+    type: String,
+  },
+  page: {
+    type: Number,
+  },
+});
+</script>
+
+<style scoped>
+.slidev-layout {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+}
+</style>
