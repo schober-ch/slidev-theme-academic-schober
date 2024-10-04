bacd39de0c1ff51b5918890e885794e6b47f39d5
Christoph Schober <mail@christoph-schober.de>
2024-10-04 21:02:33 +0200

    chore: remove submodule

--- components/Bloch.vue
+++ components/Bloch.vue
@@ -1,33 +1,17 @@
 <template>
-<canvas ref="bjsCanvas" width="500" height="500" />
-
+<GrokBloch />
 </template>
 
 
 
 <script setup lang="ts">
-import { createScene } from '../dependencies/grok-bloch/js/scene.js'
-import { ref, onMounted, computed } from 'vue'
+import { computed } from 'vue'
+import { GrokBloch } from "vue-grok-bloch"
 
 const props = defineProps<{
   scale?: number
 }>()
 
 const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
-
-const bjsCanvas = ref(null);
-
-
-const config = {
-    devicePixelRatio: window.devicePixelRatio || 1.0,
-    fontSize: 28 * devicePixelRatio
-};
-
-
-onMounted(() => {
-      if (bjsCanvas.value) {
-        createScene(bjsCanvas.value, config);
-      }
-    });
 
 </script>
