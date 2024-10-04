102cfabb32996fe04d3dea83b9012506e0ec46f3
Christoph Schober <mail@christoph-schober.de>
2024-10-04 10:35:10 +0200

    feat: update components, start adding bloch

---
+++ components/Bloch.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe src="https://javafxpert.github.io/grok-bloch/" class="w-full h-full"
+     :style="scale ? { transform: `scale(${scale})`, transformOrigin: 'top left' } : {}"
+
+        sandbox="allow-scripts allow-same-origin allow-presentation"
+>
+  </iframe>
+</template>
+
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  scale?: number
+}>()
+
+const scaleInvertPercent = computed(() => `${(1 / (props.scale || 1)) * 100}%`)
+</script>
---
+++ components/Figure.vue
@@ -0,0 +1,17 @@
+<template>
+  <figure class="flex flex-col items-center justify-center">
+    <img :alt="caption" class="max-h-full" :src="resolveAssetUrl(url)" />
+    <figcaption class="mt-3 text-center text-xs" v-if="caption">
+      <a target="_blank" v-if="sourceUrl" :href="sourceUrl">{{ caption }}</a><span v-else>{{ caption }}</span><sup v-if="footnoteNumber">{{ footnoteNumber }}</sup>
+    </figcaption>
+    <figcaption class="mt-3 text-center text-xs" v-if="html">
+      <span v-html="html"/>
+      </figcaption>
+  </figure>
+</template>
+
+<script setup lang="ts">
+import { resolveAssetUrl } from '../layout-helper';
+
+defineProps<{ caption?: string; footnoteNumber?: number; html?:string; sourceUrl?: string; url: string }>();
+</script>
